0cd441e9afd8d226ea817442646e1f8e624e2009
James Mack <wolfbro@gmail.com>
2020-12-10 17:55:28 -0800

    Update FAQ

--- js/faq.js
+++ js/faq.js
@@ -22,7 +22,8 @@ var faqContent = {
                 },
                 {
                     "question": "Do you have any pets?",
-                    "answer": "Yes, I have two cats, Asha and Keira. You can check them out on my <a href='https://www.instagram.com/wulphbro'>Instagram</a>."
+                    "answer": "Yes, I have two cats, Asha and Keira. You can check them out on " 
+                        + "my <a href='https://www.instagram.com/wulphbro'>Instagram</a>."
                 }
             ]
         },
@@ -31,7 +32,11 @@ var faqContent = {
             "questions": [
                 {
                     "question": "How long have you been streaming?",
-                    "answer": "I've streamed on and off since 2011 just to my friends. On my birthday in June 2020 I dind't know what to do so I decided I'd spend my birthday streaming Dead By Daylight.<br><br>It was a lot of fun so I started doing it every weekend. A month later I was laid off from my job and I started doing it full time. Currently I work full time and stream part time."
+                    "answer": "I've streamed on and off since 2011 just to my friends. On my birthday "
+                        + "in June 2020 I dind't know what to do so I decided I'd spend my birthday streaming "
+                        + "Dead By Daylight.<br><br>It was a lot of fun so I started doing it every weekend. "
+                        + "A month later I was laid off from my job and I started doing it full time. "
+                        + "Currently I work full time and stream part time. "
                 },
                 {
                     "question": "Are you Partner?",
@@ -48,11 +53,15 @@ var faqContent = {
             "questions": [
                 {
                     "question": "How long have you been playing DBD?",
-                    "answer": "I got the game in Feb 2017 but I only played KYF. Sometime later I came back to the game and started playing killer since none of my friends played this game. I would come back to the game, farm points on doctor for a couple weeks and then quit again. This happened until the Ash vs Evil Dead DLC came out and that sucked me back in and I've been playing regularly ever since."
+                    "answer": "I got the game in Feb 2017 but I only played KYF. Sometime later I came "
+                        + "back to the game and started playing killer since none of my friends played "
+                        + "this game. I would come back to the game, farm points on doctor for a couple "
+                        + "weeks and then quit again. This happened until the Ash vs Evil Dead DLC came "
+                        + "out and that sucked me back in and I've been playing regularly ever since."
                 },
                 {
                     "question": "How many hours do you have?",
-                    "answer": "1900"
+                    "answer": "2100"
                 },
                 {
                     "question": "Are you a killer or survivor main?",
@@ -64,7 +73,9 @@ var faqContent = {
                 },
                 {
                     "question": "Do you play survivor?",
-                    "answer": "Yes but I'm not as good at survivor as I am killer. I also think killer gameplay is more interesting for streaming so I usually prioritize that unless I'm in a SWF mood or too tilted to play anymore killer."
+                    "answer": "Yes but I'm not as good at survivor as I am killer. I also think killer "
+                        + "gameplay is more interesting for streaming so I usually prioritize that unless "
+                        + "I'm in a SWF mood or too tilted to play anymore killer."
                 },
                 {
                     "question": "Favorite Survivor?",
@@ -76,7 +87,7 @@ var faqContent = {
                 },
                 {
                     "question": "What are your favorite killers?",
-                    "answer": "Nurse, Huntress, Cannibal, Doctor, Deathslinger."
+                    "answer": "Nurse, Huntress, Cannibal, Oni."
                 },
                 {
                     "question": "What are you least favorite killers?",
@@ -90,7 +101,9 @@ var faqContent = {
             "questions": [
                 {
                     "question": "Should I use Plaid Flannel to learn Nurse?",
-                    "answer": "I strongly think no. It may help learn blinking in the short term but I believe it will be harder to learn to play without the crutch than to just learn without it from the beginning."
+                    "answer": "I strongly think no. It may help learn blinking in the short term but I "
+                        + "believe it will be harder to learn to play without the crutch than to just "
+                        + "learn without it from the beginning."
                 },
             ]
         }
